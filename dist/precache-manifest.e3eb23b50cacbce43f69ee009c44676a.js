91129eb3cde6842c49c3a2e30dfc25df879c46dc
Hristijan Draganovski <hristijan.draganovski@students.finki.ukim.mk>
2019-08-06 19:53:47 +0200

    Github pages setup

---
+++ dist/precache-manifest.e3eb23b50cacbce43f69ee009c44676a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<connected-how>/robots.txt"
+  },
+  {
+    "revision": "720c7801701507ebbefa",
+    "url": "/<connected-how>/js/chunk-vendors.d61b0bea.js"
+  },
+  {
+    "revision": "f83ee8104cde4747ab59",
+    "url": "/<connected-how>/js/app.5ca5f28d.js"
+  },
+  {
+    "revision": "98f9ba7480b720d53d38",
+    "url": "/<connected-how>/js/about.2531c7bc.js"
+  },
+  {
+    "revision": "2b3cc235accf4e197b3a08fd92a2df92",
+    "url": "/<connected-how>/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/<connected-how>/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f83ee8104cde4747ab59",
+    "url": "/<connected-how>/css/app.ed4a70a1.css"
+  }
+];
