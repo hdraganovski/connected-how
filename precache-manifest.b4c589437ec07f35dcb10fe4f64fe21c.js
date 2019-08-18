07e4cc4970e8b55a6c68d83ae3a18b13aa5c807f
Hristijan Draganovski <hristijan.draganovski@students.finki.ukim.mk>
2019-08-18 19:25:20 +0200

    Page update

---
+++ precache-manifest.b4c589437ec07f35dcb10fe4f64fe21c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c490eea21b24cd5125c",
+    "url": "/connected-how/js/chunk-vendors.b80be720.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/connected-how/robots.txt"
+  },
+  {
+    "revision": "4bcfe14a901194822224",
+    "url": "/connected-how/js/app.8d9324f3.js"
+  },
+  {
+    "revision": "fcb30bf47a3083251194",
+    "url": "/connected-how/js/about.0c34623a.js"
+  },
+  {
+    "revision": "1de1f443d7bf1777b32c6465f1777074",
+    "url": "/connected-how/index.html"
+  },
+  {
+    "revision": "bbe7ec1e33525a917d1b952f35bad097",
+    "url": "/connected-how/img/bg3.bbe7ec1e.svg"
+  },
+  {
+    "revision": "e296ee281c4cd778f50d2b3ec410715d",
+    "url": "/connected-how/img/bg2.e296ee28.svg"
+  },
+  {
+    "revision": "eb136a8058a2704f3406839a94832b7c",
+    "url": "/connected-how/img/bg1.eb136a80.svg"
+  },
+  {
+    "revision": "9c490eea21b24cd5125c",
+    "url": "/connected-how/css/chunk-vendors.56a9e82f.css"
+  },
+  {
+    "revision": "4bcfe14a901194822224",
+    "url": "/connected-how/css/app.75fb43a7.css"
+  },
+  {
+    "revision": "fcb30bf47a3083251194",
+    "url": "/connected-how/css/about.d1688a07.css"
+  }
+];
